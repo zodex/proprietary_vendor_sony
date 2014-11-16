e6d8c4c371c310e40514d4a9ac76c5283ba74f31
Michał <mimirower45@wp.pl>
2014-11-16 20:27:43 +0100

    Delete

--- msm8226-common/Android.mk
+++ msm8226-common/Android.mk
@@ -32,16 +32,4 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_PREBUILT)
 
 endif
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := sony
-LOCAL_SRC_FILES := proprietary/app/com.qualcomm.location.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := platform
-include $(BUILD_PREBUILT)
-
-endif
 endif
